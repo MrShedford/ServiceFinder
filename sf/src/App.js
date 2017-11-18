955d363f26348149e0102143a19c868fc8ab6c46
Deirdre Hegarty <deirdre@Deirdres-MacBook-Pro.local>
2017-11-18 10:16:21 +0000

    basic layout

--- sf/src/App.js
+++ sf/src/App.js
@@ -1,18 +1,30 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; //needed for bootstrap to work (& terminal :npm install --save bootstrap)
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './FS.svg';                                   //import the logo
 import './App.css';
+import {Button} from 'react-bootstrap';
+import {Row} from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
+import {Grid} from 'react-bootstrap';
+import {xs} from 'react-bootstrap';
+import {md} from 'react-bootstrap';
+import {iframe} from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Row>
+          <Col md={3} id="sideNav">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title"></h1>
+          </Col>
+        <Col md={12}>
+        </Col>
+        <Col md={9}>
+        <iframe src="//www.google.com/maps/embed/v1/place?q=Harrods,Brompton%20Rd,%20UK&zoom=17&key=AIzaSyCdv5ReoRkSbxe1pw2yRBELP7mupCW-UgY"></iframe>
+        </Col>
+        </Row>
       </div>
     );
   }
