21b9d412be63fda227daddd8d5b63cee806fbda2
Deirdre Hegarty <deirdre@Deirdres-MacBook-Pro.local>
2017-11-18 11:06:07 +0000

    sidebar under way

--- sf/src/App.js
+++ sf/src/App.js
@@ -1,4 +1,5 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; //needed for bootstrap to work (& terminal :npm install --save bootstrap)
+import $ from 'jquery'; 
 import React, { Component } from 'react';
 import logo from './FS.svg';                                   //import the logo
 import './App.css';
@@ -15,13 +16,13 @@ class App extends Component {
     return (
       <div className="App">
         <Row>
-          <Col md={3} id="sideNav">
+          <Col id="sideNav">
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title"></h1>
+            <div class="arrow-left"></div>
           </Col>
         <Col md={12}>
-        </Col>
-        <Col md={9}>
+        <div class="jumbotron"></div>
         <iframe src="//www.google.com/maps/embed/v1/place?q=Harrods,Brompton%20Rd,%20UK&zoom=17&key=AIzaSyCdv5ReoRkSbxe1pw2yRBELP7mupCW-UgY"></iframe>
         </Col>
         </Row>
